1362d6059a358e65d31b8118d2d761e39219fbb7
Tatiana Kasiyanik <t.kasiyanik@gmail.com>
2023-04-14 14:22:34 +0200

    feat: complete carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || (sampleActivity > 0 && Math.floor(sampleActivity) === +sampleActivity)) {
+    return false;
+  }
+  let activity = parseInt(sampleActivity);
+  let a = Math.log(MODERN_ACTIVITY / activity);
+  let k = 0.693 / HALF_LIFE_PERIOD;
+
+  return Math.ceil(a / k);
 }
 
 module.exports = {
