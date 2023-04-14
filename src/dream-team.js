fe81dc561c140aacf8856cbf38bc11e422cd86ab
Tatiana Kasiyanik <t.kasiyanik@gmail.com>
2023-04-14 17:44:44 +0200

    feat: complete dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (Array.isArray(members) !== true) {
+    return false;
+  }
+  let name = '';
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] !== 'string') {
+      continue;
+    }
+    for (let j = 0; j < members[i].length; j++) {
+      if (members[i][j] === ' ') {
+        continue;
+      } else {
+        name += members[i][j].toUpperCase();
+        break;
+      }
+    }
+  }
+  return name.split('').sort().join('');
 }
 
 module.exports = {
